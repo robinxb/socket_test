a4eb9f7649475d744471088d9749543359c96bc9
robin <robinxiaobin@gmail.com>
2015-09-30 16:34:48 +0800

    login meican

--- flask_templates/modules.py
+++ flask_templates/modules.py
@@ -1,10 +1,47 @@
-""" Views redirecting to the appropriate HTML file content. (Flask Routes) """
+# -*- coding: utf-8 -*-
 
 from flask_templates import app
 from flask.ext.socketio import SocketIO, emit
+from flask import request
+import requests
+import copy
 
 socketio = SocketIO(app)
+
+headers = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+           'Accept-Encoding': 'gzip, deflate',
+           'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6,zh-TW;q=0.4',
+           'Cache-Control': 'no-cache',
+           'Connection': 'keep-alive',
+           'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:22.0) Gecko/20100101 Firefox/22.0',
+           'Host': 'meican.com',
+           'Origin': 'https://meican.com',
+           'Upgrade-Insecure-Requests': 1
+           }
+
 
 @socketio.on('chat_push')
 def on_chat_recieve(message):
     emit('chat_recieve', {'content': message['data']}, broadcast=True)
+
+@app.route('/login_meican', methods=["POST"])
+def login_meican():
+    if request.method == 'POST':
+        data = request.get_json()
+        if not data["acc"] or not data["pwd"]:
+            return ""
+        payload = {'username': data["acc"], 'password': data['pwd'], 'remember':"true", 'loginType': 'username', "redirectUrl": ""}
+        with requests.Session() as s:
+            s.headers.update(headers)
+            s.get('https://meican.com/login')
+            r = s.post('https://meican.com/account/directlogin',data=payload)
+
+            r2 = s.post("https://meican.com/preorder/api/v2.1/calendarItems/list",  cookies = s.cookies, data={
+                "beginDate": "2015-9-30",
+                "endDate": "2015-10-7"
+            })
+            print r2.text
+        return ""
+    else:
+        return ""
+
--- flask_templates/static/main.js
+++ flask_templates/static/main.js
@@ -1,18 +1,45 @@
+var socket = null
+
 function append_chat_content(msg){
-    $('#chat_area').append('<p><span>' + msg + '</span></p>');
+    var d = new Date()
+    t = d.toLocaleTimeString();
+    $('#chat_area').prepend('<p><span>' + t + ":" + msg + '</span></p>');
 }
 
-$(document).ready(function(){
+function connect(){
+    append_chat_content("正在连接..");
     var socket = io.connect('http://' + document.domain + ':' + location.port, {"timeout": 2000});
     socket.on('connect', function(msg) {
-        append_chat_content("你已进入聊天");
+        append_chat_content("服务器已连接");
+        socket.emit('connect_cb');
     });
     socket.on('chat_recieve', function(msg) {
-        console.log(msg);
         append_chat_content(msg.content);
     });
     $('form#chat').submit(function(event) {
         socket.emit('chat_push', {data: $('#chat_data').val()});
         return false;
     });
-});
+};
+
+
+angular.module('meican_helper', [])
+  .controller('MeicanLoginController', function($scope, $http) {
+    var helper = this;
+    var login_data = null
+    helper.is_login = function(){
+        return login_data != null
+    }
+    helper.login = function() {
+        console.log(helper.login_psw)
+        var data = JSON.stringify({acc: helper.login_acc, pwd: helper.login_psw})
+        $http({
+            url: "/login_meican",
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            data: data})
+        .success(function(data) {
+            console.log(data)
+        });
+    };
+  });
